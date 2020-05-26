dbeb3a437433c26fea6c3c51122b0ceca65b3f2a
AndriiShnurkov <Shans12.84@ukr.net>
2020-05-26 10:38:20 +0300

    added createMessage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './styles.css';
+import axios from 'axios';
+
+
+const messageForm = document.forms.messageForm;
+
+const createMessage = (e) => {
+  e.preventDefault();
+  const message = messageForm.elements.message.value;
+
+  // fetch('https://chat-74d03.firebaseio.com/messanger.json', {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify({ message })
+  // }).then(data=> data.json()).then(data=> console.log(data))
+
+  // // console.dir(axios)
+  axios.post('https://chat-74d03.firebaseio.com/messanger.json', { message })
+    .then(response => console.log(response.data));
+
+  messageForm.reset();
+
+}
+
+messageForm.addEventListener('submit', createMessage)
+
+
+
+
+
+
+
+
+
+
+
+
+const getMessages = () => {
+
+}
+const deleteMessage = () => {
+
+}
+const updateMessage = () => {
+
+}
